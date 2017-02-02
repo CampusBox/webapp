1258689af81aa9e289b337953e4bb5d0d1aa6f07
lakshit anand <lakshit1001@gmail.com>
2017-02-03 03:10:38 +0530

    added login page and restructured the code

---
+++ bower_components/angularjs-social-login/README.md
@@ -0,0 +1,72 @@
+# AngularJS Social Login (socialLogin)
+AngularJS Social Login Module is a simple client side authentication module which helps to authenticate your application using Google/Facebook/LinkedIN. It doesn't maintain any session, session between client application and server should be maintained by yourself after receiving user details from the provider.
+
+Supported sites:
+- Google
+- Facebook
+- LinkedIN
+
+## Installation
+
+### via npm
+
+```shell
+npm install angularjs-social-login --save
+```
+
+### via bower
+
+```shell
+bower install angularjs-social-login --save
+```
+
+### configure installation
+
+Include JS files:
+
+```html
+<script src="bower_components/angularjs-social-login/angularjs-social-login.js"></script>
+```
+
+Then include `socialLogin` as a dependency for your app:
+
+```javascript
+angular.module('myApp', ['socialLogin']);
+```
+
+## Configuration
+
+### Example
+
+```javascript
+app.config(function(socialProvider){
+	socialProvider.setGoogleKey("YOUR GOOGLE CLIENT ID");
+  socialProvider.setLinkedInKey("YOUR LINKEDIN CLIENT ID");
+  socialProvider.setFbKey({appId: "YOUR FACEBOOK APP ID", apiVersion: "API VERSION"});
+});
+```
+
+## Usage
+There are total three directives for handling Google, Facebook, LinkedIn authentication.
+- fbLogin (For Facebook)
+- gLogin (For Google)
+- linkedIn (For LinkedIn)
+
+### Methods
+
+- `socialProvider.setGoogleKey("YOUR GOOGLE CLIENT ID")`
+- `socialProvider.setLinkedInKey("YOUR LINKEDIN CLIENT ID")`
+- `socialProvider.setFbKey("YOUR FACEBOOK APP ID")`
+- `$rootScope.$on('event:social-sign-in-success', function(event, userDetails){})` 
+   Braodcast event which will be triggered after successful authentication. `userDetails` is an `Object` consists of `{name: <user_name>, email: <user_email>, imageUrl: <image_url>, uid: <UID by social vendor>, provider: <Google/Facebook/LinkedIN>, token: <Google ID token (only for google)>}` 
+- `socialLoginService.logout()`
+   For logout
+- `$rootScope.$on('event:social-sign-out-success', function(event, logoutStatus){})`
+   Braodcast event which will be triggered after successful logout.
+
+### Example
+```html
+<button g-login type="button">Google Login</button>
+<button linked-in type="button">LinkedIn Login</button>
+<button fb-login type="button">facebook Login</button>
+```
--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('angularMaterialAdmin', ['ngAnimate', 'ngCookies', 'ngFileUpload',
-    'ngSanitize', 'ui.router', 'ngMaterial', 'nvd3', 'app', 'angular-medium-editor'
+    'ngSanitize', 'ui.router', 'ngMaterial', 'nvd3', 'app', 'angular-medium-editor', 'socialLogin'
 ])
 
 .config(function($stateProvider, $urlRouterProvider, $mdThemingProvider,
-    $mdIconProvider) {
+    $mdIconProvider, socialProvider) {
     $stateProvider
         .state('home', {
             url: '',
@@ -14,79 +14,33 @@ angular.module('angularMaterialAdmin', ['ngAnimate', 'ngCookies', 'ngFileUpload'
             controllerAs: 'vm',
             abstract: true
         })
-        .state('home.dashboard', {
+        .state('static', {
+            url: '',
+            templateUrl: 'app/views/static.html',
+            controller: 'StaticController',
+            controllerAs: 'vm',
+            abstract: true
+        })
+
+
+    .state('static.login', {
+        url: '/login',
+        templateUrl: 'app/views/static/login.html',
+        data: {
+            title: 'Dashboard'
+        }
+    })
+
+    .state('home.dashboard', {
             url: '/dashboard',
-            templateUrl: 'app/views/dashboard.html',
+            templateUrl: 'app/views/home/dashboard.html',
             data: {
                 title: 'Dashboard'
             }
         })
-        .state('home.profile', {
-            url: '/profile',
-            templateUrl: 'app/views/profile.html',
-            controller: 'ProfileController',
-            controllerAs: 'vm',
-            data: {
-                title: 'Profile'
-            }
-        })
-        .state('home.events', {
-            url: '/events',
-            templateUrl: 'app/views/events.html',
-            controller: 'EventsController',
-            controllerAs: 'vm',
-            data: {
-                title: 'Profile'
-            }
-        })
-        .state('home.myEvents', {
-            url: '/myEvents',
-            templateUrl: 'app/views/myEvents.html',
-            controller: 'EventsController',
-            controllerAs: 'vm',
-            data: {
-                title: 'My Events'
-            }
-        })
-        .state('home.myBlogs', {
-            url: '/myBlogs',
-            templateUrl: 'app/views/myBlogs.html',
-            controller: 'BlogsController',
-            controllerAs: 'vm',
-            data: {
-                title: 'My Blogs'
-            }
-        })
-        .state('home.addBlog', {
-            url: '/addBlog',
-            templateUrl: 'app/views/addBlog.html',
-            controller: 'AddBlogController',
-            controllerAs: 'vm',
-            data: {
-                title: 'Add a Post'
-            }
-        })
-        .state('home.societies', {
-            url: '/societies',
-            templateUrl: 'app/views/societies.html',
-            controller: 'SocietiesController',
-            controllerAs: 'vm',
-            data: {
-                title: 'Profile'
-            }
-        })
-        .state('home.blogs', {
-            url: '/blogs',
-            templateUrl: 'app/views/blogs.html',
-            controller: 'BlogsController',
-            controllerAs: 'vm',
-            data: {
-                title: 'Profile'
-            }
-        })
         .state('home.search', {
             url: '/search',
-            templateUrl: 'app/views/search.html',
+            templateUrl: 'app/views/home/search.html',
             data: {
                 title: 'Dashboard'
             }
@@ -95,13 +49,83 @@ angular.module('angularMaterialAdmin', ['ngAnimate', 'ngCookies', 'ngFileUpload'
             url: '/table',
             controller: 'TableController',
             controllerAs: 'vm',
-            templateUrl: 'app/views/table.html',
+            templateUrl: 'app/views/home/table.html',
             data: {
                 title: 'Table'
+            }
+        })
+        .state('home.profile', {
+            url: '/profile',
+            templateUrl: 'app/views/home/profile.html',
+            controller: 'ProfileController',
+            controllerAs: 'vm',
+            data: {
+                title: 'Profile'
+            }
+        })
+
+
+
+    .state('home.events', {
+            url: '/events',
+            templateUrl: 'app/views/home/events.html',
+            controller: 'EventsController',
+            controllerAs: 'vm',
+            data: {
+                title: 'Profile'
+            }
+        })
+        .state('home.myEvents', {
+            url: '/myEvents',
+            templateUrl: 'app/views/home/myEvents.html',
+            controller: 'EventsController',
+            controllerAs: 'vm',
+            data: {
+                title: 'My Events'
+            }
+        })
+
+    .state('home.blogs', {
+            url: '/blogs',
+            templateUrl: 'app/views/home/blogs.html',
+            controller: 'BlogsController',
+            controllerAs: 'vm',
+            data: {
+                title: 'Profile'
+            }
+        })
+        .state('home.myBlogs', {
+            url: '/myBlogs',
+            templateUrl: 'app/views/home/myBlogs.html',
+            controller: 'BlogsController',
+            controllerAs: 'vm',
+            data: {
+                title: 'My Blogs'
+            }
+        })
+        .state('home.addBlog', {
+            url: '/addBlog',
+            templateUrl: 'app/views/home/addBlog.html',
+            controller: 'AddBlogController',
+            controllerAs: 'vm',
+            data: {
+                title: 'Add a Post'
+            }
+        })
+        .state('home.societies', {
+            url: '/societies',
+            templateUrl: 'app/views/home/societies.html',
+            controller: 'SocietiesController',
+            controllerAs: 'vm',
+            data: {
+                title: 'Profile'
             }
         });
 
     $urlRouterProvider.otherwise('/dashboard');
+    socialProvider.setGoogleKey("702228530885-vi264d7g6v5ivbcmebjfpomr0hmliomd.apps.googleusercontent.com");
+    socialProvider.setLinkedInKey("YOUR LINKEDIN CLIENT ID");
+    socialProvider.setFbKey({ appId: "1250377088376164", apiVersion: "v2.8" });
 
     $mdIconProvider
         .defaultIconSet('assets/svg/mdi.svg');
