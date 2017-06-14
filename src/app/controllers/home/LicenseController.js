47cad5ccb5066ad03a3b2ed005a3a577e04d7f8e
Akash Sharma <akashsharms@gmail.com>
2017-06-14 17:34:18 +0530

    testing single license

---
+++ src/app/controllers/home/LicenseController.js
@@ -0,0 +1,34 @@
+(function(){
+'use strict';
+	angular.module('app')
+          .controller('LicenseController',['$scope','$mdDialog',LicenseController]);
+
+         function LicenseController ($scope, $mdDialog) {
+
+
+			
+            $scope.showCustom = function($event) {
+             
+               //actual dialog
+               $mdDialog.show({
+                  clickOutsideToClose: true,
+                  scope: $scope,        
+                  preserveScope: true,
+                  targetEvent: $event,           
+                  templateUrl: 'app/views/partials/license.html',
+                  controller: SelectLicenseController
+               });
+
+               function SelectLicenseController($scope, $mdDialog) {
+                     $scope.closeDialog = function() {
+                        console.log('CLOSING DIALOG');
+                        $mdDialog.hide();
+                     };
+               } 
+            }
+
+          
+            }
+ 
+
+})();//this is a comment
