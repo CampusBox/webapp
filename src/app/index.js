b1f7770bcb3d5b572d464ec556a1c9826168d6e2
Lakshit1001 <Lakshit1001@ymail.com>
2017-06-16 12:01:01 +0530

    2: Auto stash before merge of "test2" and "test"

--- src/app/index.js
+++ src/app/index.js
@@ -22,9 +22,9 @@ angular.module('angularMaterialAdmin', ['ngAnimate', 'satellizer',
 
          */
 
-        // $compileProvider.debugInfoEnabled(false);
-        // $compileProvider.commentDirectivesEnabled(false);
-        // $compileProvider.cssClassDirectivesEnabled(false);
+        $compileProvider.debugInfoEnabled(false);
+        $compileProvider.commentDirectivesEnabled(false);
+        $compileProvider.cssClassDirectivesEnabled(false);
         // $locationProvider.html5Mode(true);
 
 
