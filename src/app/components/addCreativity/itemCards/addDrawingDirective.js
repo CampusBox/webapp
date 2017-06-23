c510625ec07d0bd442e9397375b9ccd73fc1fd9b
goelrohan6 <goelrohan6@gmail.com>
2017-06-23 14:15:10 +0530

    Pulling and setting url from instagram

--- src/app/components/addCreativity/itemCards/addDrawingDirective.js
+++ src/app/components/addCreativity/itemCards/addDrawingDirective.js
@@ -7,7 +7,7 @@
             restrict: "E",
             replace: true,
             templateUrl: 'app/components/addCreativity/itemCards/addDrawing.html',
-            controller: function($scope, addItemService, $sce) {
+            controller: function($scope, addItemService, $sce, allDataService) {
                 //Define Variables
                 $scope.allowedDrawing = [4, 5];
                 $scope.musicAdded = false;
@@ -22,42 +22,29 @@
 
                     }
                 }
+
                 $scope.instagram = function() {
                     var url = 'https://www.instagram.com/p/BVpn4AfHqXJ/?r=wa1';
-                    // var result = url.replace(/(^\w+:|^)\/\//, '');
                     url = url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "");
                     url = 'https://' + url;
+
+                    allDataService.noembedJson(url)
+                        .then(function(data) {
+                            var media = {};
+                            media.mediaType = 'image';
+                            media.image = data.thumbnail_url;
+                            $scope.creativity.items.push(media);
+                            $scope.creativity.items[1].noembed = data;
+                            if ($scope.title == '') {
+                                $scope.title = $scope.creativity.items[1].noembed.title;
+                            }
+                        });
                 }
                 $scope.removeItem = function(index) {
                     $scope.creativity.items.splice(index, 1);
                     $scope.musicAdded = false;
                 }
-                $scope.addDrawing = function(url) {
-                    if ($scope.validateYoutube(url)) {
-                        if ($scope.creativity.items.length > 1) {
-                            $scope.creativity.items.pop();
-                        }
-                        $scope.setNoembed(url);
-                        $scope.checkVideo();
-                        addItemService.youtube(url);
-                    } else if ($scope.validateSoundcloud(url)) {
-                        if ($scope.creativity.items.length > 1) {
-                            $scope.creativity.items.pop();
-                        }
-                        addItemService.soundcloud(url);
-                        $scope.setNoembed(url);
-                        $scope.checkVideo();
-                    } else if ($scope.validateVimeo(url)) {
-                        if ($scope.creativity.items.length > 1) {
-                            $scope.creativity.items.pop();
-                        }
-                        addItemService.vimeo(url);
-                        $scope.setNoembed(url);
-                        $scope.checkVideo();
-                    } else {
-                        $scope.error = 'Enter a valid Youtube, Soundcloud or Vimeo url.'
-                    }
-                };
+
             }
         };
     });
