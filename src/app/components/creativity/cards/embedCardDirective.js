20955363c4ada5c3ee77e9ce545c9373fc64bda3
goelrohan6 <goelrohan6@gmail.com>
2017-07-01 01:54:15 +0530

    View type full embed done

---
+++ src/app/components/creativity/cards/embedCardDirective.js
@@ -0,0 +1,22 @@
+(function() {
+    'use strict';
+
+    angular.module('app').
+    directive('embedCard', function() {
+        return {
+            restrict: "E",
+            replace: true,
+            templateUrl: 'app/components/creativity/cards/embedCard.html',
+            scope: {
+                bar: '=bar',
+                content: '=data'
+            },
+            controller: function($scope, $sce) {
+                console.log($scope.content);
+                // $scope.content.items.embed = $sce.trustAsHtml($scope.content.items.embed);
+
+            }
+        };
+    });
+
+})();
