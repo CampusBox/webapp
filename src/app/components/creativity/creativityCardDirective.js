6dc20b6af1c6bdb2c1d7adab91d3fe3c4f11540a
goelrohan6 <goelrohan6@gmail.com>
2017-06-30 21:06:52 +0530

    Creativity shit management cleaning

---
+++ src/app/components/creativity/creativityCardDirective.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+
+    angular.module('app').
+    directive('creativityCard', function() {
+        return {
+            restrict: "E",
+            replace: true,
+            templateUrl: 'app/components/creativity/creativityCard.html',
+
+            controller: function() {
+
+
+
+
+            }
+        };
+    });
+
+})();
