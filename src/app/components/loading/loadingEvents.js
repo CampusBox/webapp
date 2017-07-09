aa1fb6228eaa4093976723b23fb4e185283343d2
Ayush <ayushpahwa96@gmail.com>
2017-07-09 20:15:34 +0530

    Fixed events list UI

--- src/app/components/loading/loadingEvents.js
+++ src/app/components/loading/loadingEvents.js
@@ -3,10 +3,10 @@
     angular.module('app').
     directive('loadingEvents', function() {
         return {
-        	restrict: "E",
-            templateUrl: 'app/components/events/loadingEvents/loadingEvents.html',
-             controller: function( $scope) {
-                 $scope.eventLoading = true;
+            restrict: "E",
+            templateUrl: 'app/components/loading/loadingEvents.html',
+            controller: function($scope) {
+                $scope.eventLoading = true;
             }
         };
     });
